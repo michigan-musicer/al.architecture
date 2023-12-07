66d5baded81c3aa25cfa16ec1c1929d40dac3f72
michigan-musicer <musicer@umich.edu>
2023-12-07 22:48:38 +0100

    add OS content

---
+++ os/examples.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
+
+// This commented-out example is taken from CMU: https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
+// You can find more code at the link above for examples of pthread_join and other pthread functionality.
+
+// void *print_message_function( void *ptr )
+// {
+//      char *message;
+//      message = (char *) ptr;
+//      printf("%s \n", message);
+// }
+
+// int main()
+// {
+//      pthread_t thread1, thread2;
+//      char *message1 = "Thread 1";
+//      char *message2 = "Thread 2";
+//      int  iret1, iret2;
+
+//     /* Create independent threads each of which will execute function */
+
+//      iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+//      iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+
+//      /* Wait till threads are complete before main continues. Unless we  */
+//      /* wait we run the risk of executing an exit which will terminate   */
+//      /* the process and all threads before the threads have completed.   */
+
+//      pthread_join( thread1, NULL);
+//      pthread_join( thread2, NULL); 
+
+//      printf("Thread 1 returns: %d\n",iret1);
+//      printf("Thread 2 returns: %d\n",iret2);
+//      exit(0);
+// }
+
+void create_file() 
+{
+    open("./new_file.txt", O_CREAT);
+}
+
+void *print_one() 
+{
+    printf("1 ");
+}
+
+int main() 
+{
+    pthread_t thread1, thread2;
+    int i = 0;
+    int iret1 = pthread_create( &thread1, NULL, print_one, (void*)(&i) );
+    int iret2 = pthread_create( &thread2, NULL, print_one, (void*)(&i) );
+    pthread_join( thread1, NULL);
+    pthread_join( thread2, NULL);
+}
